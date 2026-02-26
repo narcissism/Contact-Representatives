a3e376366b6c0eac80b33cdcb6ec9b0cece595c2
narcissism <narcissism@users.noreply.github.com>
2026-02-26 14:04:24 -0500

    Create data.js

---
+++ data.js
@@ -0,0 +1,12 @@
+const MEMBERS = [
+  {
+    name:"Example Senator",
+    position:"Senate",
+    state:"NY",
+    party:"D",
+    committees:"Finance",
+    pac_total:"$2,300,000",
+    dc_phone:"202-555-1111",
+    state_phone:"212-555-2222"
+  }
+]
